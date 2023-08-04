9121d5882a0bb99ecf188dec51f9ebbeef427465
afreddo <freddoa@live.com>
2023-08-04 13:13:28 -0500

    Initial Commit

---
+++ NameMemorizer/NameMemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameMemorizerApp.swift
+//  NameMemorizer
+//
+//  Created by Alex Freddo on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct NameMemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
